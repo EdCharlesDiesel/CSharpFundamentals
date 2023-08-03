5050d494ecb7989273969448c0d651e94834797f
Khotso <Mokhetkc@hotmail.com>
2023-08-03 14:56:48 +0200

    Initial commit.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
